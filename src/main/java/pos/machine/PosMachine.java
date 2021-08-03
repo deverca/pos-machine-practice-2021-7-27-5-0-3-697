e5e7c4742bf25237e2251f70d06f4c7da7608398
deverca <deverca@oocl.com>
2021-08-03 20:19:44 +0800

    [POSMachine][Carmela] feat: added calculateQty and generateProductDetails

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -1,9 +1,27 @@
 package pos.machine;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static pos.machine.ItemDataLoader.loadAllItemInfos;
 
 public class PosMachine {
+
+    public List<ItemInfo> generateProductDetails(String barcode){
+
+        return loadAllItemInfos().stream()
+                .filter(itemInfo -> itemInfo.getBarcode().equalsIgnoreCase(barcode)).collect(Collectors.toList());
+
+    }
+
+    public int calculateQty(List<ItemInfo> productDetails){
+
+        return productDetails.size();
+    }
+
+
     public String printReceipt(List<String> barcodes) {
-        return null;
+
+     return null;
     }
 }
