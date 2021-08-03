3243956a6e2402ebd4c7289e326facbd7ca5fb94
deverca <deverca@oocl.com>
2021-08-03 20:52:22 +0800

    [POSMachine][Carmela]feat: modified printReceipt

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -1,5 +1,6 @@
 package pos.machine;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,35 +8,45 @@ import static pos.machine.ItemDataLoader.loadAllItemInfos;
 
 public class PosMachine {
 
-    public List<ItemInfo> generateProductDetails(String barcode){
+    public List<ItemInfo> generateProductDetails(String barcode) {
 
         return loadAllItemInfos().stream()
                 .filter(itemInfo -> itemInfo.getBarcode().equalsIgnoreCase(barcode)).collect(Collectors.toList());
 
     }
 
-    public int calculateQty(List<ItemInfo> productDetails){
+    public int calculateQty(List<ItemInfo> productDetails) {
 
         return productDetails.size();
     }
 
-    public double calculateSubTotal(double price, int quantity){
+    public double calculateSubTotal(double price, int quantity) {
 
         return price * quantity;
     }
-    public double calculateTotal(List<Double> subTotals){
-        return subTotals.stream().mapToDouble(a->a).sum();
+
+    public double calculateTotal(List<Double> subTotals) {
+        return subTotals.stream().mapToDouble(a -> a).sum();
     }
 
-    public String generateReceiptDetails(int quantity, double subtotal, List<ItemInfo> productDetails){
-        return "Name: "+productDetails.get(0).getName()+", Quantity: "+quantity
-                +", Unit price: " + productDetails.get(0).getPrice() + "(yuan), Subtotal: "+ subtotal + "(yuan)\n";
+    public String generateReceiptDetails(int quantity, double subtotal, List<ItemInfo> productDetails) {
+        return "Name: " + productDetails.get(0).getName() + ", Quantity: " + quantity
+                + ", Unit price: " + productDetails.get(0).getPrice() + "(yuan), Subtotal: " + subtotal + "(yuan)\n";
 
     }
 
 
     public String printReceipt(List<String> barcodes) {
-
-     return null;
+        StringBuilder receipt = new StringBuilder("***<store earning no money>Receipt ***\n");
+        List<Double> subTotals = new ArrayList<>();
+        for (String barcode : barcodes) {
+            List<ItemInfo> productDetails = generateProductDetails(barcode);
+            int quantity = calculateQty(productDetails);
+            double subTotal = calculateSubTotal(productDetails.get(0).getPrice(), quantity);
+            subTotals.add(subTotal);
+            receipt.append(generateReceiptDetails(quantity, subTotal, productDetails));
+        }
+        receipt.append("Total: ").append(calculateTotal(subTotals)).append(" (yuan)");
+        return receipt.toString();
     }
 }
